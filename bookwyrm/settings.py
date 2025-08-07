4784dc81547b0f4e94354c6f1d6caafcecb1af36
Ian Young <ian@iangreenleaf.com>
2025-08-06 23:29:36 -0500

    Use WhiteNoise to serve static files

--- bookwyrm/settings.py
+++ bookwyrm/settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -494,7 +495,7 @@ else:
             "BACKEND": "django.core.files.storage.FileSystemStorage",
         },
         "staticfiles": {
-            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
         },
         "exports": {
             "BACKEND": "django.core.files.storage.FileSystemStorage",
